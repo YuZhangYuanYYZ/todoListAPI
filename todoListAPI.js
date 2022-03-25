8abc7a830f1bca585d6f62c1ee94180380f65551
yyz <yuanyuzhangyyzz@163.com>
2022-03-25 23:06:46 +0800

    solve the cors

--- todoListAPI.js
+++ todoListAPI.js
@@ -5,6 +5,18 @@ import express from "express";
 import bodyParser from "body-parser"
 const app = express();
 app.use(bodyParser.json());
+app.all("*",function(req,res,next){
+  //设置允许跨域的域名，*代表允许任意域名跨域
+  res.header("Access-Control-Allow-Origin","*");
+  //允许的header类型
+  res.header("Access-Control-Allow-Headers","content-type");
+  //跨域允许的请求方式
+  res.header("Access-Control-Allow-Methods","DELETE,PUT,POST,GET,OPTIONS");
+  if (req.method.toLowerCase() == 'options')
+    res.send(200);  //让options尝试请求快速结束
+  else
+    next();
+});
 
 const store = new Store();
 app.get('/todos', function (req, res) {
